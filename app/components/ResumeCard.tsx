20b043f00df4a77d1530e59b84320374e6b71a16
SOUNAK GHOSH <sounak2309@gmail.com>
2025-08-03 10:12:52 +0530

    added interfaces, constants and css classes

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -12,18 +12,17 @@ const ResumeCard = ({
             className="resume-card animate-in fade-in duration-1000"
         >
             <div className="resume-card-header">
-                
-            <div className="flex flex-col gap-2">
-                <h2 className="!text-black font-bold break-words">
-                    {companyName}
-                </h2>
-                <h3 className="text-lg break-words text-gray-500">
-                    {jobTitle}
-                </h3>
-            </div>
-            <div className="flex-shrink-0">
-                <ScoreCircle score={feedback.overallScore} />
-            </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="!text-black font-bold break-words">
+                        {companyName}
+                    </h2>
+                    <h3 className="text-lg break-words text-gray-500">
+                        {jobTitle}
+                    </h3>
+                </div>
+                <div className="flex-shrink-0">
+                    <ScoreCircle score={feedback.overallScore} />
+                </div>
             </div>
             {resumeUrl && (
                 <div className="gradient-border animate-in fade-in duration-1000">
@@ -35,7 +34,7 @@ const ResumeCard = ({
                         />
                     </div>
                 </div>
-                )}
+            )}
         </Link>
     );
 };
